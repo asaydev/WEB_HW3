e74a7dd8c617511f6e681ca0369c5682cf511c85
Ali Emadi <ali7ev@gmail.com>
2021-01-11 21:12:04 +0330

    some changes

--- frontend/AdminPanel/src/js/script.js
+++ frontend/AdminPanel/src/js/script.js
@@ -2,22 +2,22 @@
 // const first_num = document.getElementById("fi").value;
 // const second_num = document.getElementById("si").value;
 // const find_form = document.getElementById("find_form");
-const register_form = document.getElementById("reg-form");
-const login_form = document.getElementById("login-form");
+// const register_form = document.getElementById("reg-form");
+// const login_form = document.getElementById("login-form");
+const baseUrl = "http://192.168.1.101/api"
 
 function registerr() {
     alert('bye');
-    document.getElementById("reg-form").addEventListener("submit", (ev => {
+    document.getElementById("reg-form").addEventListener("submit", (e => {
         alert('hi');
         // e.preventDefault(); // ?
         const request = new XMLHttpRequest();
-        request.open("post", "http://192.168.1.101/api/signup", true);
+        request.open("post", baseUrl + "/signup", true);
         console.log(`email=${document.getElementById("inputEmail4").value}`);
         request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         request.onreadystatechange = function () { // Call a function when the state changes.
             if (this.readyState === XMLHttpRequest.DONE && this.status === 201) {
-                location.replace("http://192.168.1.101");
-                alert(request.responseText)
+                alert(request.responseText);
             }
             if (this.readyState === XMLHttpRequest.DONE && this.status !== 201) {
                 alert(this.status)
@@ -28,17 +28,17 @@ function registerr() {
 }
 
 function loginn() {
-    document.getElementById("login-form").addEventListener("submit", (ev => {
+    document.getElementById("login-form").addEventListener("submit", (e => {
         e.preventDefault(); // ?
         let request = new XMLHttpRequest();
-        request.open("post", "http://192.168.1.101/api/signup", true);
+        request.open("post", baseUrl + "/signup", true);
         request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         request.onreadystatechange = function () { // Call a function when the state changes.
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
 
             }
             if (this.readyState === XMLHttpRequest.DONE && this.status !== 200) {
-                // not 200
+
             }
             alert(this.status)
         };
@@ -46,3 +46,28 @@ function loginn() {
     }))
 }
 
+function createPost() {
+    document.getElementById("createPostForm").addEventListener("submit", e => {
+        e.preventDefault();
+        let request = new XMLHttpRequest();
+        request.open("post", baseUrl + "/admin/post/crud", true);
+        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.onreadystatechange = function () {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+
+            }
+            if (this.readyState === XMLHttpRequest.DONE && this.status !== 200) {
+
+            }
+            alert(this.status)
+
+        };
+        request.send(`title=${document.getElementById("titleInput").value}&content=${document.getElementById("captionInput").value}`)
+
+    })
+
+}
+
+function getPost() {
+    
+}
